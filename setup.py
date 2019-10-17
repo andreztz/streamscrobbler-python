886d565215908462a22e80f4df329dec09ad3a74
André P. Santos <andreztz@gmail.com>
2019-10-17 14:07:46 -0300

    Ver. "0.0.4".

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@
 from setuptools import setup
 
 setup(
-    name="streamscrobbler",
+    name="streamscrobbler3",
     packages=["streamscrobbler"],
-    version="0.0.2",
+    version="0.0.4",
     description="A python class used on Dirble.com to get titles and bitrates on shoutcast and icecast streams",
     author="Håkan Nylén",
     author_email="confacted@gmail.com",
-    url="https://github.com/Dirble/streamscrobbler-python",
+    url="https://github.com/andreztz/streamscrobbler-python",
     keywords=["shoutcast", "icecast", "stream", "ICY"],
     install_requires=[],
     classifiers=[],
-    include_package_data=True
+    include_package_data=True,
 )
